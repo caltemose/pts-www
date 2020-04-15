873ffd484b500e7f4be00c640b139ef69443e293
Chad Altemose <chad@chadzilla.com>
2020-04-15 14:59:23 -0400

    sass and browsersync working

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,11 @@
 const { series, parallel, watch, src, dest } = require('gulp')
 const del = require('del')
 const pug = require('gulp-pug')
+const gulpif = require('gulp-if')
+const sass = require('gulp-sass')
+const autoprefixer = require('gulp-autoprefixer')
+const cleanCSS = require('gulp-clean-css')
+const browsersync = require('browser-sync').create()
 
 const _src = 'src/'
 const _dest = 'dist'
@@ -16,9 +21,6 @@ const config = {
 	sass: {
 		src: _src + 'assets/sass/**/*.sass',
 		dest: _dest + '/assets/css'
-	},
-	autoprefixer: {
-		options: { browsers: ["last 3 version"] }
 	},
 	js: {
 		src: [ _src + 'assets/js/pages/**/*.js' ],
@@ -50,12 +52,42 @@ function html () {
 	return src(config.pug.src)
 		.pipe(pug(pugOptions))
 		.pipe(dest(config.dest))
+		.pipe(browsersync.stream())
 }
 
+function css () {
+	return src(config.sass.src)
+		.pipe(sass({ indentedSyntax: true }))
+		.pipe(autoprefixer())
+		.pipe(gulpif(isProductionEnv, cleanCSS({ compatibility: 'ie8' })))
+		.pipe(dest(config.sass.dest))
+		.pipe(browsersync.stream())
+}
+
+function serve (cb) {
+	browsersync.init({
+		server: { baseDir: './' + config.dest },
+		port: config.port,
+		notify: false,
+		open: true
+	})
+	cb()
+}
+
+function watchAll () {
+	watch(config.pug.src, html)
+	watch(config.sass.src, css)
+}
 
 exports.build = series(
 	clean,
-	parallel(html)
+	parallel(html, css)
+)
+
+exports.dev = series(
+	exports.build,
+	series(serve, watchAll)
 )
 
 exports.html = html
+exports.css = css
