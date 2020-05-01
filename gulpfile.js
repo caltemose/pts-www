8c18ca1fc65ff9e6b2357d2eb6746a470072656a
Chad Altemose <chad@chadzilla.com>
2020-05-01 15:23:15 -0400

    removed docs folder - now using dist folder; added production script

--- gulpfile.js
+++ gulpfile.js
@@ -1,24 +1,41 @@
+// primary build system tool: gulpjs.com
 const { series, parallel, watch, src, dest } = require('gulp')
+
+// basic utilities
 const del = require('del')
-const pug = require('gulp-pug')
 const gulpif = require('gulp-if')
-const sass = require('gulp-sass')
-const autoprefixer = require('gulp-autoprefixer')
-const cleanCSS = require('gulp-clean-css')
-const imagemin = require('gulp-imagemin')
-const browsersync = require('browser-sync').create()
 const changed = require('gulp-changed')
 
+// html templating language https://pugjs.org/
+const pug = require('gulp-pug')
+
+// css preprocessor https://sass-lang.com/
+const sass = require('gulp-sass')
+const autoprefixer = require('gulp-autoprefixer')
+
+// development server utility https://www.browsersync.io/
+const browsersync = require('browser-sync').create()
+
+// javascript utilities so we can write ES6+ and transpile it down to
+// ES5 for browser support
 const rollupEach = require('gulp-rollup-each')
 const rollupBuble = require('@rollup/plugin-buble')
 const globals = require('rollup-plugin-node-globals')
 const builtins = require('rollup-plugin-node-builtins')
 const resolve = require('@rollup/plugin-node-resolve')
 const commonjs = require('@rollup/plugin-commonjs')
+
+// utilities for minifying and prepping for production
+const cleanCSS = require('gulp-clean-css')
+const imagemin = require('gulp-imagemin')
 const uglify = require('gulp-uglify')
 
+
+const ENV_DEV = 'development'
+const ENV_PROD = 'production'
+
 const _src = 'src/'
-const _dest = 'docs'
+const _dest = 'dist'
 
 const config = {
 	port: 4300,
@@ -41,9 +58,6 @@ const config = {
 		dest: _dest + '/assets/img'
 	}
 }
-
-const ENV_DEV = 'development'
-const ENV_PROD = 'production'
 
 function isProductionEnv () {
 	return getNodeEnv() === ENV_PROD
@@ -120,23 +134,35 @@ function serve (cb) {
 
 function watchAll () {
 	watch(config.pug.src, html)
+	// this watch is added so that when includes files are changed, 
+	// the html is rebuilt. 
 	watch(_src + 'html/_includes/**/*', html)
 	watch(config.sass.src, css)
 	watch(config.images.src, images)
 	watch(config.js.src, js)
+	// this watch is added so that when the module files change,
+	// the javascript is rebuilt
 	watch(_src + 'assets/js/modules/**/*', js)
 }
 
+/*
+these gulp tasks are exported so they can be used by 
+our package.json script and node.
+*/
+
+// empty the distribution folder and then run the
+// primary build tasks in parallel. if the node environment
+// is the production environment, these tasks will compress and
+// minify files as needed.
 exports.build = series(
 	clean,
 	parallel(html, css, images, js)
 )
 
+// the development task run with `npm start` which builds files,
+// runs the browsersync server and watches files for changes to
+// rebuild them
 exports.dev = series(
 	exports.build,
 	series(serve, watchAll)
 )
-
-exports.html = html
-exports.css = css
-exports.js = js
