e65a03ddcc9a6cddc1d6d9345bcbd1c643a61b86
Chad Altemose <chad@chadzilla.com>
2020-06-03 09:49:56 -0400

    added subpages

--- src/assets/js/pages/index.js
+++ src/assets/js/pages/index.js
@@ -8,3 +8,41 @@ if (urlParams) {
 
 const $form = new ContactForm(document.querySelector('.contact-form'));
 $form.init(testing);
+
+// const $heroChart = document.getElementById('hero-dynamic');
+
+const data = [
+	{
+		x: ['Morningside-Lenox Park', 'Virginia-Highland', 'Old Fourth Ward', 'Reynoldstown', 'Inman Park'],
+		y: [6,7,13,9,11],
+		type: 'bar',
+		marker: {
+			color: [
+				'rgb(237, 125, 49)', // ml
+				'rgb(68, 114, 196)', // vh
+				'rgb(165, 165, 165)', // o4
+				'rgb(255, 192, 1)', // re
+				'rgb(91, 154, 213)' // ip
+			]
+		}
+	}
+];
+
+const layout = {
+	showlegend: false
+};
+
+const config = {
+	displayModeBar: false,
+	staticPlot: true
+};
+
+if (Plotly) Plotly.newPlot('hero-dynamic', data, layout, config);
+
+/*
+Morningside-Lenox Park	6%
+Virginia-Highland	7%
+Old Fourth Ward	13%
+Reynoldstown	9%
+Inman Park	11%
+*/
